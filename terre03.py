edb8db833142f712e0948caab89066c0d0d1537a
samuel-risch <rischebourg.samuel@gmail.com>
2023-11-03 19:08:49 +0100

    L’alphabet à partir de

---
+++ terre03.py
@@ -0,0 +1,14 @@
+import sys
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+lettre_alphabet = sys.argv[1]
+
+for lettre in alphabet[alphabet.index(lettre_alphabet):]:
+    print(lettre)
+
+    
+
+
+
+    
