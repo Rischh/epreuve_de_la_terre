480218b7d9391d59e4127d5919abc403f3b4ed4e
samuel-risch <rischebourg.samuel@gmail.com>
2023-11-10 01:46:14 +0100

    Modification

--- terre02.py
+++ terre02.py
@@ -1,4 +1,16 @@
+# import sys
+
+# for arg in sys.argv[1:]:
+#     print(arg)
+    
 import sys
 
-for arg in sys.argv[1:]:
-    print(arg)
+
+def display_arguments(user_arg):
+    for arg in user_arg:           # Fonction
+        print(arg)
+    
+user_arguments = sys.argv[1:]      # Parsing    
+
+        
+display_arguments(user_arguments)  # Affichage 
--- terre03.py
+++ terre03.py
@@ -1,11 +1,22 @@
-import sys
+# import sys
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-lettre_alphabet = sys.argv[1]
+# lettre_alphabet = sys.argv[1]
 
-for lettre in alphabet[alphabet.index(lettre_alphabet):]:
-    print(lettre)
+# for lettre in alphabet[alphabet.index(lettre_alphabet):]:
+#     print(lettre)
+
+import sys 
+
+def alphabet_from(alpha):
+    for letter in alpha[alpha.index(lettre):]: # Fonction
+        print(letter)
+
+lettre = sys.argv[1]
+alphabet = "abcdefghijklmnopqrstuvwxyz"        # Parsing  
+
+alphabet_from(alphabet)                        # Affichage 
 
     
 
