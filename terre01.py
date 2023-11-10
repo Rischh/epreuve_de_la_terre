e6d571649150694e56f1ad7194ae8a0d06ac59fb
samuel-risch <rischebourg.samuel@gmail.com>
2023-11-10 01:43:41 +0100

    Rectification

--- terre01.py
+++ terre01.py
@@ -1,3 +1,24 @@
-fichier = str(__file__)
-fichier = fichier.split("/", 5)
-print(fichier[5])
+# Premier code:
+
+# fichier = str(__file__)
+# fichier = fichier.split("/", 5)
+# print(fichier[5])
+
+import os
+
+def script_name():
+    file_name = os.path.basename(__file__) # Fonction
+    print(file_name)                          
+
+script_name()                              # Affichage 
+
+# Scop local
+
+    
+
+
+
+
+
+    
+    
