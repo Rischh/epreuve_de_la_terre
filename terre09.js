aabbe8fcf1e59719d5c4567d96b36bafad782c15
Samuel Rischebourg <rischebourg.samuel@gmail.com>
2025-01-04 16:05:42 +0100

    Refactor terre09.py

--- terre09.js
+++ terre09.js
@@ -1,13 +1,11 @@
 const { argv } = require("node:process");
 
 const squareRoot = (arg) => {
-  for (let i = 0; i < arg; i++)
-    if (i ** 2 === arg) {
-      return i;
-    }
+  for (let i = 0; i < arg; i++) if (i ** 2 === arg) return i;
 };
 
 const arg = Number(argv.splice(2)[0]);
 
 const result = squareRoot(arg);
+
 console.log(result);
