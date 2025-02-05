60d78f8eef13c5f3060b01ca48d429cdec005c8f
Samuel Rischebourg <rischebourg.samuel@gmail.com>
2025-02-05 09:42:19 +0100

    refactor:terre12: errorhandling + scope global

--- terre12.js
+++ terre12.js
@@ -1,24 +1,32 @@
-const { argv } = require("node:process");
+const arguments = process.argv.splice(2);
+const argument = arguments[0];
 
-const toTwentyFourHourFormat = (arg) => {
-  const list = arg.split("");
+const timeFormat12HRegex = /^([0][0-9]|[1][0-2]):([0-5][0-9])(AM|PM)$/;
 
-  const hour = list[0] + list[1];
-  const minutes = list[3] + list[4];
-  const meridian = list[5] + list[6];
-  const meridianAM = "AM";
-  const meridianPM = "PM";
+if (arguments.length !== 1) {
+  console.error("Le programme à besoin de 1 argument pour fonctionner.");
+  process.exit();
+}
 
-  if (hour == 12 && meridian == meridianPM) twentyFourHour = 12;
-  else if (hour == 12 && meridian == meridianAM) twentyFourHour = "00";
-  else if (meridian == meridianAM) twentyFourHour = hour;
-  else twentyFourHour = Number(hour) + 12;
+if (argument.search(timeFormat12HRegex) === -1) {
+  console.error(`Le programme à besoin d'un argument en format 12H:
+hh:mmAM`);
+  process.exit();
+}
 
-  return `${twentyFourHour}:${minutes}`;
-};
+const array = argument.split("");
 
-const arg = argv.splice(2)[0];
+const hour = array[0] + array[1];
+const minutes = array[3] + array[4];
+const meridian = array[5] + array[6];
+const meridianAM = "AM";
+const meridianPM = "PM";
 
-const result = toTwentyFourHourFormat(arg);
+if (hour == 12 && meridian == meridianPM) twentyFourHour = 12;
+else if (hour == 12 && meridian == meridianAM) twentyFourHour = "00";
+else if (meridian == meridianAM) twentyFourHour = hour;
+else twentyFourHour = Number(hour) + 12;
 
-console.log(result);
+const twentyFourFormat = `${twentyFourHour}:${minutes}`;
+
+console.log(twentyFourFormat);
