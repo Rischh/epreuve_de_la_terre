b7e75fc3642e059792a613f6853386ec3c0cf07c
Samuel Rischebourg <rischebourg.samuel@gmail.com>
2025-02-05 09:16:32 +0100

    refactor:terre11: errorhandling + scope global

--- terre11.js
+++ terre11.js
@@ -1,38 +1,46 @@
-const { argv } = require("node:process");
+const arguments = process.argv.splice(2);
 
-const toTwelveHourFormat = (arg) => {
-  const list = arg.split("");
+const argument = arguments[0];
 
-  const hour = list[0] + list[1];
-  const minutes = list[3] + list[4];
-  const meridianAM = "AM";
-  const meridianPM = "PM";
+const timeFormat24HRegex = /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/;
 
-  let twelveHour = null;
-  let meridian = "";
+if (arguments.length !== 1) {
+  console.error("Le programme à besoin de 1 argument pour fonctionner.");
+  process.exit();
+}
 
-  if (hour == 12) {
-    twelveHour = 12;
-    meridian = meridianPM;
-  } else if (hour == 0) {
-    twelveHour = 12;
-    meridian = meridianAM;
-  } else if (hour == 22 || hour == 23) {
-    twelveHour = hour - 12;
-    meridian = meridianPM;
-  } else if (hour < 12) {
-    twelveHour = hour;
-    meridian = meridianAM;
-  } else {
-    twelveHour = "0" + (hour - 12);
-    meridian = meridianPM;
-  }
+if (argument.search(timeFormat24HRegex) === -1) {
+  console.error(`Le programme à besoin d'un argument en format 24H:
+HH:MM`);
+  process.exit();
+}
 
-  return `${twelveHour}:${minutes}${meridian}`;
-};
+const array = argument.split("");
 
-const arg = argv.splice(2)[0];
+const hour = array[0] + array[1];
+const minutes = array[3] + array[4];
+const meridianAM = "AM";
+const meridianPM = "PM";
 
-const result = toTwelveHourFormat(arg);
+let twelveHour, meridian;
 
-console.log(result);
+if (hour == 12) {
+  twelveHour = 12;
+  meridian = meridianPM;
+} else if (hour == 0) {
+  twelveHour = 12;
+  meridian = meridianAM;
+} else if (hour == 22 || hour == 23) {
+  twelveHour = hour - 12;
+  meridian = meridianPM;
+} else if (hour < 12) {
+  twelveHour = hour;
+  meridian = meridianAM;
+} else {
+  twelveHour = "0" + (hour - 12);
+  meridian = meridianPM;
+}
+
+const twelveHourFormat = `${twelveHour}:${minutes}${meridian}`;
+
+console.log(twelveHourFormat);
