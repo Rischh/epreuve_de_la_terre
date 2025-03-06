dfab460f3126356171babe0c15f5defe40ca187e
Rischh <rischebourg.samuel@gmail.com>
2025-03-06 14:00:16 +0100

    refactor: terre11 && terre12

--- terre11.js
+++ terre11.js
@@ -1,46 +1,41 @@
-const arguments = process.argv.splice(2);
+const args = process.argv.splice(2);
 
-const argument = arguments[0];
-
-const timeFormat24HRegex = /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/;
-
-if (arguments.length !== 1) {
-  console.error("Le programme à besoin de 1 argument pour fonctionner.");
+if (args.length !== 1) {
+  console.error("Le programme a besoin d'un unique argument pour fonctionner.");
   process.exit();
 }
 
-if (argument.search(timeFormat24HRegex) === -1) {
-  console.error(`Le programme à besoin d'un argument en format 24H:
+const arg = args[0];
+const timeFormat24HRegex = /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/;
+
+if (!timeFormat24HRegex.test(arg)) {
+  console.error(`Le programme a besoin d'un argument en format 24H:
 HH:MM`);
   process.exit();
 }
 
-const array = argument.split("");
-
-const hour = array[0] + array[1];
-const minutes = array[3] + array[4];
+const hours = Number(arg[0] + arg[1]);
+const minutes = Number(arg[3] + arg[4]);
 const meridianAM = "AM";
 const meridianPM = "PM";
 
 let twelveHour, meridian;
 
-if (hour == 12) {
+if (hours === 12) {
   twelveHour = 12;
   meridian = meridianPM;
-} else if (hour == 0) {
+} else if (hours === 0) {
   twelveHour = 12;
   meridian = meridianAM;
-} else if (hour == 22 || hour == 23) {
-  twelveHour = hour - 12;
+} else if (hours === 22 || hours === 23) {
+  twelveHour = hours - 12;
   meridian = meridianPM;
-} else if (hour < 12) {
-  twelveHour = hour;
+} else if (hours < 12) {
+  twelveHour = hours;
   meridian = meridianAM;
 } else {
-  twelveHour = "0" + (hour - 12);
+  twelveHour = "0" + (hours - 12);
   meridian = meridianPM;
 }
 
-const twelveHourFormat = `${twelveHour}:${minutes}${meridian}`;
-
-console.log(twelveHourFormat);
+console.log(`${twelveHour}:${minutes}${meridian}`);
--- terre12.js
+++ terre12.js
@@ -1,32 +1,28 @@
-const arguments = process.argv.splice(2);
-const argument = arguments[0];
+const args = process.argv.splice(2);
 
-const timeFormat12HRegex = /^([0][0-9]|[1][0-2]):([0-5][0-9])(AM|PM)$/;
-
-if (arguments.length !== 1) {
-  console.error("Le programme à besoin de 1 argument pour fonctionner.");
+if (args.length !== 1) {
+  console.error("Le programme a besoin d'un unique argument pour fonctionner.");
   process.exit();
 }
 
-if (argument.search(timeFormat12HRegex) === -1) {
-  console.error(`Le programme à besoin d'un argument en format 12H:
+const timeFormat12HRegex = /^([0][0-9]|[1][0-2]):([0-5][0-9])(AM|PM)$/;
+const arg = args[0];
+
+if (!timeFormat12HRegex.test(arg)) {
+  console.error(`Le programme a  besoin d'un argument en format 12H:
 hh:mmAM`);
   process.exit();
 }
 
-const array = argument.split("");
-
-const hour = array[0] + array[1];
-const minutes = array[3] + array[4];
-const meridian = array[5] + array[6];
+const hours = Number(arg[0] + arg[1]);
+const minutes = Number(arg[3] + arg[4]);
+const meridian = Number(arg[5] + arg[6]);
 const meridianAM = "AM";
 const meridianPM = "PM";
 
-if (hour == 12 && meridian == meridianPM) twentyFourHour = 12;
-else if (hour == 12 && meridian == meridianAM) twentyFourHour = "00";
-else if (meridian == meridianAM) twentyFourHour = hour;
-else twentyFourHour = Number(hour) + 12;
+if (hours === 12 && meridian === meridianPM) twentyFourHour = 12;
+else if (hours === 12 && meridian === meridianAM) twentyFourHour = "00";
+else if (meridian === meridianAM) twentyFourHour = hours;
+else twentyFourHour = hours + 12;
 
-const twentyFourFormat = `${twentyFourHour}:${minutes}`;
-
-console.log(twentyFourFormat);
+console.log(`${twentyFourHour}:${minutes}`);
