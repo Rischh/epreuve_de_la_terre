cd5008ce8baa446acfc10c07b105685a45cb07ce
Rischh <rischebourg.samuel@gmail.com>
2025-03-06 13:59:25 +0100

    refactor: terre13

--- terre13.js
+++ terre13.js
@@ -1,38 +1,30 @@
-const arguments = process.argv.splice(2);
+const args = process.argv.splice(2);
 
-if (arguments.length !== 3) {
-  console.error("Le programme à besoin de 3 argument pour fonctionner.");
+if (args.length !== 3) {
+  console.error("Le programme a besoin de 3 arguments pour fonctionner.");
   process.exit();
 }
 
-const numbersToValidate = arguments.map(value => +value);
-const hasInvalidNumbers =
-  numbersToValidate.filter(value => Number.isNaN(value)).length > 0;
+let a = args[0];
+let b = args[1];
+let c = args[2];
 
-const bashSpecialCharacter = [""]; // not all
-
-for (let i = 0; i < arguments.length; i++) {
-  if (hasInvalidNumbers || bashSpecialCharacter.includes(arguments[i])) {
-    console.error("L'argument doit être un entier.");
-    process.exit();
-  }
-  for (let j = i + 1; j < arguments.length; j++) {
-    if (arguments[i] === arguments[j]) {
-      console.error("Les arguments ne doivent pas être égaux.");
-      process.exit();
-    }
-  }
+if (
+  a === "" ||
+  b === "" ||
+  c === "" ||
+  !Number.isInteger(+a) ||
+  !Number.isInteger(+b) ||
+  !Number.isInteger(+c)
+) {
+  console.error("Le programme a besoin de 3 entiers.");
+  process.exit();
 }
 
-let max = 0;
+a = +a;
+b = +b;
+c = +c;
 
-for (value of arguments) if (max < value) max = value;
-
-let middle = 0;
-
-for (value of arguments) {
-  if (value == max) continue;
-  if (middle < value) middle = value;
-}
-
-console.log(middle);
+if ((a > b && a < c) || (a > c && a < b)) console.log(a);
+else if ((b > a && b < c) || (b > c && b < a)) console.log(b);
+else console.log(c);
