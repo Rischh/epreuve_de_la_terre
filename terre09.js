cd16efc00afa5359233803ad4657143d257c8dbd
Rischh <rischebourg.samuel@gmail.com>
2025-03-06 13:18:40 +0100

    refactor: terre09
    - Add the exit of the program for an error handling

--- terre09.js
+++ terre09.js
@@ -5,19 +5,22 @@ if (args.length !== 1) {
   process.exit();
 }
 
-const arg = parseInt(args[0]);
+const arg = args[0];
 
-if (isNaN(arg)) {
-  console.error("Le programme a besoin d'un nombre.");
+if (arg === "" || !Number.isInteger(+arg)) {
+  console.error("Le programme a besoin d'un entier.");
   process.exit();
 }
 
-if (arg < 0) {
-  console.error("Le nombre doit être un entier positif.");
+const number = +arg;
+
+if (number < 0) {
+  console.error("L'entier doit être positif.");
+  process.exit();
 }
 
-for (let i = 0; i <= arg; i++)
-  if (i ** 2 === arg) {
+for (let i = 0; i < number; i++)
+  if (i ** 2 === number) {
     console.log(i);
     break;
   }
