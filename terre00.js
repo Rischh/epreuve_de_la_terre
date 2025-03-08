8dd9797d3d8a15a3d39b6540efd8843183853d11
Rischh <rischebourg.samuel@gmail.com>
2025-03-08 17:28:42 +0100

    refactor[terre00]: Add more context to the loop

--- terre00.js
+++ terre00.js
@@ -1,6 +1,9 @@
 let alphabet = ""
 
-for (let i = 97; i <= 122; i++) {
+const LOWERCASE_A_CHAR_CODE = 97
+const UPPERCASE_Z_CHAR_CODE = 122
+
+for (let i = LOWERCASE_A_CHAR_CODE; i <= UPPERCASE_Z_CHAR_CODE; i++) {
   alphabet += String.fromCharCode(i)
 }
 
