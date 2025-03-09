5d2ea5d76038aa5b229bff6467d2b6c6aad6c15d
Rischh <rischebourg.samuel@gmail.com>
2025-03-09 15:28:11 +0100

    refactor[terre09]: Add a resolution message
    - splice to slice
    - Delete arg
    - Use isNaN
    - Add a message for number who don't have an exaxt square root

--- terre09.js
+++ terre09.js
@@ -1,26 +1,26 @@
-const args = process.argv.splice(2)
+const args = process.argv.slice(2)
 
 if (args.length !== 1) {
   console.error("Le programme a besoin d'un unique argument pour fonctionner.")
   process.exit()
 }
 
-const arg = args[0]
-
-if (arg === "" || !Number.isInteger(+arg)) {
-  console.error("Le programme a besoin d'un entier.")
+if (isNaN(args[0])) {
+  console.error("Le programme a besoin d'un nombre entier.")
   process.exit()
 }
 
-const number = +arg
+const number = +args[0]
 
 if (number < 0) {
-  console.error("L'entier doit être positif.")
+  console.error("Le nombre doit être un entier positif.")
   process.exit()
 }
 
-for (let i = 0; i < number; i++)
-  if (i ** 2 === number) {
+for (let i = 0; i < number; i++) {
+  if (i * i === number) {
     console.log(i)
-    break
+    process.exit()
   }
+}
+console.log("Le nombre n'a pas de racine carré exact.")
