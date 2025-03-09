23fe8b327377f66351052ca8cb1acea13a1e9f47
Rischh <rischebourg.samuel@gmail.com>
2025-03-09 11:54:36 +0100

    refactor[terre08]: Revert to a loop for the calcul
    - Replace splice to slice
    - Delete 'firstArg' & 'secondArg'
    - Loop for a error handling
    - Rename 'exposant'
    - Loop for the resolution

--- terre08.js
+++ terre08.js
@@ -1,31 +1,34 @@
-const args = process.argv.splice(2)
+const args = process.argv.slice(2)
 
 if (args.length !== 2) {
   console.error("Le programme a besoin de 2 argument pour fonctionner.")
   process.exit()
 }
 
-const firstArg = args[0]
-const secondArg = args[1]
-
-if (firstArg === "" || secondArg === "" || isNaN(firstArg) || isNaN(secondArg)) {
-  console.error("Les arguments doivent être des nombres.")
-  process.exit()
+for (const arg of args) {
+  if (isNaN(arg)) {
+    console.error("Les arguments doivent être des nombres.")
+    process.exit()
+  }
 }
 
-const base = +firstArg
-const exposant = +secondArg
+const base = +args[0]
+const exponant = +args[1]
 
-if (exposant < 0) {
+if (exponant < 0) {
   console.error("L'exposant doit être un nombre positif.")
   process.exit()
 }
 
-if (base === 0 && exposant === 0) {
+if (base === 0 && exponant === 0) {
   console.error("Le puissance de 0 sur 0 est une expression mathematiquement indeterminée.")
   process.exit()
 }
 
-let power = base ** exposant
+let power = base
+
+for (let i = 1; i < exponant; i++) {
+  power *= base
+}
 
 console.log(power)
