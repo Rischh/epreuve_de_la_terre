e77bf2c1c794e6aa5bb7001892e71839aa013540
Rischh <rischebourg.samuel@gmail.com>
2025-03-10 08:51:50 +0100

    refactor[terre14]: Add a boolean variable
    - Replace splice to slice
    - Replace for to for...of
    - Implement a boolean variable to split between the algo and the display

--- terre14.js
+++ terre14.js
@@ -1,4 +1,4 @@
-const args = process.argv.splice(2)
+const args = process.argv.slice(2)
 
 if (args.length < 2) {
   console.error("Le programme a besoin d'au moins 2 arguments.")
@@ -7,17 +7,19 @@ if (args.length < 2) {
 
 const numbers = []
 
-for (let i = 0; i < args.length; i++) {
-  if (args[i] === "" || !Number.isInteger(+args[i])) {
-    console.error("Le programme a besoin d'une liste d'entiers.")
-    process.exit()
-  } else numbers.push(+args[i])
-}
-
-for (let i = 1; i < numbers.length; i++)
-  if (numbers[i] < numbers[i - 1]) {
-    console.log("Pas triée !")
+for (const arg of args) {
+  if (isNaN(arg)) {
+    console.error("Le programme a besoin d'une liste de nombres entiers.")
     process.exit()
   }
+  numbers.push(+arg)
+}
 
-console.log("Triée !")
+let isSorted = true
+
+for (let i = 1; i < numbers.length; i++)
+  if (numbers[i] < numbers[i - 1]) isSorted = false
+
+if (isSorted) {
+  console.log("Triée !")
+} else console.log("Pas Triée !")
