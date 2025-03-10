9ccacd561d64b1be12f2002897e6e114512f6574
Rischh <rischebourg.samuel@gmail.com>
2025-03-10 10:01:55 +0100

    refactor[terre00]: rename a variable that has a bad name

--- terre00.js
+++ terre00.js
@@ -1,9 +1,9 @@
 let alphabet = ""
 
 const LOWERCASE_A_CHAR_CODE = 97
-const UPPERCASE_Z_CHAR_CODE = 122
+const LOWERCASE_Z_CHAR_CODE = 122
 
-for (let i = LOWERCASE_A_CHAR_CODE; i <= UPPERCASE_Z_CHAR_CODE; i++) {
+for (let i = LOWERCASE_A_CHAR_CODE; i <= LOWERCASE_Z_CHAR_CODE; i++) {
   alphabet += String.fromCharCode(i)
 }
 
